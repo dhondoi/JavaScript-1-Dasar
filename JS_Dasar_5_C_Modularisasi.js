28046a6eaf0a6ada4cddcf8d30a88a612419e894
Doni Firmansyah <donifirmansyah33@gmail.com>
2026-07-06 14:59:02 +0700

    move to repo module

--- JS_Dasar_5_C_Modularisasi.js
+++
@@ -1,8 +0,0 @@
-// di node js
-// "npm init -y" jika tidak ada
-// buka berkas _package.json_ dan tambahkan sintaks `"type": "module"`
-
-// di browser
-// <script src="./esmodule.js" type="module">
-// jika tidak support module tambahkan
-// <script nomodule src="fallback.js"></script>
